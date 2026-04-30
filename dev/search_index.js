746435ed7b0bfb79f74517c807e263aabead3b1c
Documenter.jl <documenter@juliadocs.github.io>
2026-04-30 08:10:56 +0000

    build based on 1a0715e

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#PeriodicARModels","page":"Home","title":"PeriodicARModels","text":"Documentation for PeriodicARModels.\n\nA package to use AR models with periodic parameters (often monthly) for Stochastic Weather Generators. ","category":"section"}]
+[{"category":"section","location":"#PeriodicARModels","page":"Home","text":"Documentation for PeriodicARModels.\n\nA package to use AR models with periodic parameters (often monthly) for Stochastic Weather Generators. ","title":"PeriodicARModels"}]
 }
